755a96bbd2a6a69bc817c8083c8948d6867595db
Mirza Touseef Baig <mirza.touseef.baig@macys.com>
2019-01-08 03:20:51 +0500

    Eureka client is added in app-common pom file and api gateway successfully routing to customer app

--- customer-app/src/main/java/com/fdm/customer/SpringBootKafkaConsumerExampleApplication.java
+++ customer-app/src/main/java/com/fdm/customer/SpringBootKafkaConsumerExampleApplication.java
@@ -2,8 +2,10 @@ package com.fdm.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootKafkaConsumerExampleApplication {
 
 	public static void main(String[] args) {
--- order-app/src/main/java/com/fdm/order/SpringBootKafkaConsumerExampleApplication.java
+++ order-app/src/main/java/com/fdm/order/SpringBootKafkaConsumerExampleApplication.java
@@ -1,13 +1,16 @@
 package com.fdm.order;
 
+import com.fdm.common.model.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Configuration
+@EnableEurekaClient
 public class SpringBootKafkaConsumerExampleApplication {
 
 	public static void main(String[] args) {
