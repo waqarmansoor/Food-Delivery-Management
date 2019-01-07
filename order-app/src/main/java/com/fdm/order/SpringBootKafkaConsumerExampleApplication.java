69c1f164dd26d9a5257f1882870dd783cf1c2982
waqar mansoor <Waqar.Mansoor@macys.com>
2019-01-07 23:00:17 +0500

    FDM  : Common app added

--- order-app/src/main/java/com/fdm/order/SpringBootKafkaConsumerExampleApplication.java
+++ order-app/src/main/java/com/fdm/order/SpringBootKafkaConsumerExampleApplication.java
@@ -1,6 +1,5 @@
 package com.fdm.order;
 
-import com.fdm.order.model.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
